186359fbf2308204ad39975cdf70b320e7d72460
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-02 14:48:43 +0300

    Add movies table in admin of django

--- movcom/movies/admin.py
+++ movcom/movies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Actor, Genre, Movie, MovieGenres
 
 # Register your models here.
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth')
+    search_fields = ('first_name', 'last_name')
+    
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_year', 'director', 'rating')
+    search_fields = ('title', 'director')
+    
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(MovieGenres)
