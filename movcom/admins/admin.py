29e55b7ddc9974cef733e6d728946121320885aa
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-03 16:36:15 +0300

    Adding fields of database in admin of django

--- movcom/admins/admin.py
+++ movcom/admins/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Admin, Role, Permission, AdminAction
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Role)
+admin.site.register(Permission)
+admin.site.register(AdminAction)
