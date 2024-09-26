89c9cff96c542e0bf38df1bc0973e819fbee3ffb
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-26 17:43:22 +0300

    Template users admins

--- movcom/admins/views.py
+++ movcom/admins/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def user_list(request):
+    users = User.objects.all()
+    
+    return render(request, 'admins/users.html', {'users': users})
--- movcom/movcom/urls.py
+++ movcom/movcom/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admins/', include('admins.urls')),
     path('auth/', include('authe.urls')),
     path('accounts/', include('allauth.urls')),
     path('movies/', include('movies.urls')),
