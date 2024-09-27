96b24143079e65badbee21bfd3a0336d76d22fe3
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-27 10:22:19 +0300

    views users list in admin dashboard

--- movcom/admins/views.py
+++ movcom/admins/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 # Create your views here.
 def user_list(request):
     users = User.objects.all()
+    user_count = users.count()
     
-    return render(request, 'admins/users.html', {'users': users})
+    return render(request, 'admins/users.html', {'users': users, 'user_count': user_count})
