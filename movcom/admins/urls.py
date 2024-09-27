a20ae43c4b4d70bef6908171e323ec72c14a5214
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-27 11:58:03 +0300

    Add User detail view in admin dashboard

--- movcom/admins/urls.py
+++ movcom/admins/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_list
+from .views import user_list, user_detail
 
 urlpatterns = [
     path('users/', user_list, name='user-list'),
+    path('users/detail/<int:user_id>/', user_detail, name='user-detail')
 ]
--- movcom/admins/views.py
+++ movcom/admins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -10,3 +10,8 @@ def user_list(request):
     page_obj = paginator.get_page(page_number)
     
     return render(request, 'admins/users.html', {'page_obj': page_obj})
+
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    
+    return render(request, 'admins/user-detail.html', {'user': user})
