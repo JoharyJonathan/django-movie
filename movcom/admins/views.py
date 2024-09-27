ec7743bebf408569dc899beae6eea1cd3c1555eb
JoharyJonathan <rjoharyjonathan@gmail.com>
2024-09-27 11:22:47 +0300

    Pagination for users list

--- movcom/admins/views.py
+++ movcom/admins/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def user_list(request):
-    users = User.objects.all()
-    user_count = users.count()
+    users = User.objects.all().order_by('id')
+    paginator = Paginator(users, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     
-    return render(request, 'admins/users.html', {'users': users, 'user_count': user_count})
+    return render(request, 'admins/users.html', {'page_obj': page_obj})
